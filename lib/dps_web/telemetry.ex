867619685188012c6f840dbd8b9eb801975bd699
Svyat Sobol <svyatoslav.sobol@gmail.com>
2024-09-22 22:37:16 +0300

    Add telemetry logger

--- lib/dps_web/telemetry.ex
+++ lib/dps_web/telemetry.ex
@@ -1,3 +1,56 @@
+defmodule DPSWeb.Telemetry.Logger do
+  use GenServer
+
+  require Logger
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(opts) do
+    :ok = init_telemetry()
+
+    {:ok, opts}
+  end
+
+  def init_telemetry do
+    :telemetry.attach_many(
+      __MODULE__,
+      [
+        [:dps, :topic_server, :join],
+        [:dps, :topic_server, :publish],
+        [:dps, :topic_server, :leave],
+        [:dps, :topic_client, :publish],
+        [:dps, :topic_channel, :join],
+        [:dps, :topic_channel, :publish],
+        [:dps, :topic_channel, :handle_out]
+      ],
+      &handle_event/4,
+      nil
+    )
+  end
+
+  def format_measurements(measurements) do
+    duration = Map.get(measurements, :duration, nil)
+
+    if duration do
+      %{measurements | duration: System.convert_time_unit(duration, :native, :microsecond)}
+    else
+      measurements
+    end
+  end
+
+  def handle_event(
+        event,
+        measurements,
+        metadata,
+        _config
+      ) do
+    Logger.info("#{inspect(event)} - #{inspect(measurements)} - #{inspect(metadata)}")
+  end
+end
+
 defmodule DPSWeb.Telemetry do
   use Supervisor
   import Telemetry.Metrics
@@ -8,7 +61,13 @@ defmodule DPSWeb.Telemetry do
 
   @impl true
   def init(_arg) do
-    Supervisor.init([], strategy: :one_for_one)
+    Supervisor.init(
+      [
+        # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()},
+        DPSWeb.Telemetry.Logger
+      ],
+      strategy: :one_for_one
+    )
   end
 
   def metrics do
@@ -47,7 +106,16 @@ defmodule DPSWeb.Telemetry do
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
       summary("vm.total_run_queue_lengths.total"),
       summary("vm.total_run_queue_lengths.cpu"),
-      summary("vm.total_run_queue_lengths.io")
+      summary("vm.total_run_queue_lengths.io"),
+
+      # DPS
+      counter("dps.topic_server.join.count"),
+      counter("dps.topic_server.publish.count"),
+      counter("dps.topic_server.leave.count"),
+      counter("dps.topic_client.publish.count"),
+      counter("dps.topic_channel.join.count"),
+      counter("dps.topic_channel.publish.count"),
+      counter("dps.topic_channel.handle_out.count")
     ]
   end
 end
